4ab76363b821e67fc27b6df7b05c668fc910818f
Skyrich_u <petercha2000@gmail.com>
2020-12-26 06:04:22 +0900

    update : 2020. 12. 26. (토) 06:04:22 KST

--- build.sh
+++ build.sh
@@ -3,3 +3,8 @@ cp ~/.vimrc ./res
 cp ~/.tmux.conf ./res
 cp ~/.tmux.reset.conf ./res
 cp -rf ~/.tmux/mine ./res
+
+git add .
+git commit -m "update : $(date)"
+git push
+echo "[SETTING] :: done update"
