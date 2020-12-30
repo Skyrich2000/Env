e989ab1358d1d3880d990e504524da3d1f511e34
Skyrich_u <petercha2000@gmail.com>
2020-12-31 02:59:53 +0900

    update : 2020. 12. 31. (목) 02:59:53 KST

--- build.sh
+++ build.sh
@@ -7,8 +7,13 @@ cp ~/.tmux.conf ./res
 cp ~/.tmux.reset.conf ./res
 cp -rf ~/.tmux/mine ./res
 
+message=""
+if [ $1 != "" ]; then
+	message=" | $1"
+fi
+
 git add .
-git commit -m "update : $(date)"
+git commit -m "update : $(date) $message"
 git push
 
 echo -e "$C[SETTING] :: done update$E"
