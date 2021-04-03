f70848950d109f4e4822a008b3cd011a0ec3e35b
Skyrich_u <petercha2000@gmail.com>
2021-04-03 18:45:58 +0000

    docker ssh added

--- install_docker.sh
+++ install_docker.sh
@@ -16,6 +16,7 @@ docker cp ./res/install_my_env_root.sh $1:/
 echo "$Cinstall my env in $1$E"
 docker exec -it $1 bash /install_my_env_root.sh
 echo "$Ccopy user installer to $1$E"
-docker cp ./res/install_my_env.sh $1:/home/dhuni/
+docker cp ./res/install_my_env.sh $1:/
+docker cp ~/.ssh/authorized_keys_docker $1:/
 echo "$Cinstall my env in $1$E"
-docker exec -it -u 1000 $1 sudo bash /home/dhuni/install_my_env.sh
+docker exec -it -u 1000 $1 bash /install_my_env.sh
--- res/install_my_env.sh
+++ res/install_my_env.sh
@@ -6,5 +6,14 @@ echo -e "$C[DOCKER SETTING] :: clone git$E"
 git clone https://github.com/skyrich2000/env ~/.myenv
 echo -e "$C[DOCKER SETTING] :: start docker setting$E"
 cd ~/.myenv
-./docker.sh
-rm ~/install_my_env.sh
+bash docker.sh
+echo -e "$C[DOCKER SETTING] :: start ssh setting$E"
+sudo apt install -y openssh-server
+mkdir ~/.ssh
+chmod 700 ~/.ssh
+cp /authorized_keys_docker ~/.ssh/authorized_keys
+sudo sed -i "s/^.*PubkeyAuthentication.*$/PubkeyAuthentication yes/" /etc/ssh/sshd_config
+sudo service ssh restart
+echo -e "$C[DOCKER SETTING] :: delete$E"
+sudo rm /install_my_env.sh
+sudo rm /authorized_keys_docker
