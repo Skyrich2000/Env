dbee5d504fc922a51b12f00b33f06879680ed05b
Skyrich_u <petercha2000@gmail.com>
2021-11-28 09:12:28 +0000

    Feat: docker ssh fixed

--- res/install_my_env.sh
+++ res/install_my_env.sh
@@ -7,12 +7,15 @@ git clone https://github.com/skyrich2000/env ~/.myenv
 echo -e "$C[DOCKER SETTING] :: start docker setting$E"
 cd ~/.myenv
 bash docker.sh
+echo -e "$C[DOCKER SETTING] :: change shell to zsh$E"
+echo dhuni | chsh -s /usr/bin/zsh
 echo -e "$C[DOCKER SETTING] :: start ssh setting$E"
-sudo apt install -y openssh-server
+sudo DEBIAN_FRONTEND="noninteractive" apt install -y openssh-server
 mkdir ~/.ssh
 chmod 700 ~/.ssh
 cp /authorized_keys_docker ~/.ssh/authorized_keys
 sudo sed -i "s/^.*PubkeyAuthentication.*$/PubkeyAuthentication yes/" /etc/ssh/sshd_config
+sudo sed -i "s/^.*PasswordAuthentication.*$/PasswordAuthentication no/" /etc/ssh/sshd_config
 sudo service ssh restart
 echo -e "$C[DOCKER SETTING] :: delete$E"
 sudo rm /install_my_env.sh
