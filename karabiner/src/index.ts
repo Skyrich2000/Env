28be7aefe37743ca3df99a76da2088fc72b9e025
younghoon-cha <younghoon.cha@toss.im>
2022-07-21 14:49:27 +0900

    feat: karabiner fix to internal keyboard

--- karabiner/src/index.ts
+++ karabiner/src/index.ts
@@ -4,10 +4,10 @@ import { internalConfig } from './config/Internal';
 import { Modification } from './types/Modification';
 import { exec } from 'child_process';
 
-const varmilo: Modification = new Modification(
-  varmiloConfig.title,
-  Rules(varmiloConfig),
-);
+// const varmilo: Modification = new Modification(
+//   varmiloConfig.title,
+//   Rules(varmiloConfig),
+// );
 
 const internal: Modification = new Modification(
   internalConfig.title,
@@ -15,6 +15,6 @@ const internal: Modification = new Modification(
 );
 
 // https://karabiner-elements.pqrs.org/docs/json/external-json-generators/
-const base64string = Buffer.from(JSON.stringify(varmilo)).toString('base64');
+const base64string = Buffer.from(JSON.stringify(internal)).toString('base64');
 let url = `karabiner://karabiner/assets/complex_modifications/import?url=data:application/json;charset=utf-8;base64,${base64string}`;
 exec(`open "${url}"`);
